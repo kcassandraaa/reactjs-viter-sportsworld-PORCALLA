751376348c5ec29c7c3301234a74ee689e36672d
kc <kc@gmail.com>
2025-07-15 10:39:50 +0800

    commit

--- src/components/partials/PageNotFound.jsx
+++ src/components/partials/PageNotFound.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
     <>
-      <h1>404 - Page Not Found</h1>
+      <div className="text-center">
+        <div className="mt-56 leading-[13rem] ">
+          <h1 className="font-bold text-[13rem] text-gray-600">404</h1>
+          <h2 className="font-bold text-7xl text-gray-600 ">Page Not Found</h2>
+        </div>
+        <Link
+          to="/"
+          className="mt-9 inline-block bg-blue-600 text-white text-lg font-semibold px-4 py-2 rounded hover:bg-blue-700 transition duration-300"
+        >
+          Go Home
+        </Link>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
